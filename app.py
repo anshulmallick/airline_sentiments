a621493ccbb551cbc534187bb35d6d8ef8f7ee8b
maverick24x <39401921+maverick24x@users.noreply.github.com>
2020-10-25 21:35:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ This is a temporary script file.
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
